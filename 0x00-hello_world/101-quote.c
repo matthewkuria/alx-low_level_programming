94175a6afb25b2970a12aeaf3bbec229b3350b5f
matthewkuria <mathewkwachira@gmail.com>
2023-03-17 00:51:12 -0700

    101-quote.c commit

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <unistd.h>
+/**
+ * main - A program that prints a line to a standard error
+ * Return: 1 (Success)
+ */
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return (1);
+}
