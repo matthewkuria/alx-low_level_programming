7666a3353e23a2f8376e62231b1326eb9fc43acf
matthewkuria <mathewkwachira@gmail.com>
2023-03-17 00:56:01 -0700

    101-quote.c commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,6 @@
  */
 int main(void)
 {
-fput(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	fputs(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
 	return (1);
 }
