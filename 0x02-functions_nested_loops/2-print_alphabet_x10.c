e898890de9dee28673a5ffd36cbf8a34ee573c8a
matthewkuria <mathewkwachira@gmail.com>
2023-03-21 14:20:58 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - 'Prints 10 times the alpahbets in lower case'
+ *
+ * Return: Always 0
+ */
+void print_alphabet_x10(void)
+{
+	int i = 0;
+	char ch;
+
+	while (i++ <= 9)
+	{
+		for (ch = 'a'; ch <= 'z'; ch++)
+		{
+			_putchar(ch);
+			_putchar('\n');
+		}
+	}
+}
