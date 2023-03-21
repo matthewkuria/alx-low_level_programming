66a184395b7edef03461f3c0a24cbac3055fd856
matthewkuria <mathewkwachira@gmail.com>
2023-03-21 14:56:27 -0700

    4-isalpha.c commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,20 @@
+/*
+ * File name - 4-isalpha.c
+ * Editted by :Matthew Kuria
+ */
+#include "main.h"
+/**
+ * _isalpha - Entry point
+ * @c: the character to be checked
+ *
+ * Return: 1 if c  is a letter, lowercase or uppercase otherwise 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') ||
+			(c >= 'A' && c <= 'Z'))
+		return (1);
+	else
+		return (0);
+}
+
