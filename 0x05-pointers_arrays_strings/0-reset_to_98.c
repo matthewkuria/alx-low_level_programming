3af535784ad1faa4027cff106ab28bf260ba191e
matthewkuria <mathewkwachira@gmail.com>
2023-03-27 14:01:30 -0700

    $

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * reset_to_98 - A function that takes apointer and updates its values
+ * @n:Input
+ * Return: n
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/README.md
@@ -0,0 +1,1 @@
+0x05. C - Pointers, arrays and strings Directory for alx SWE 27/3/23
